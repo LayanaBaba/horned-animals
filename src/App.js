0a2d4154a65f8bfcc6412b70c508a7905d252079
LayanaBaba <leana_baba@yahoo.com>
2021-05-18 16:41:49 +0300

    semi-solution

--- src/App.js
+++ src/App.js
@@ -7,14 +7,47 @@ import Header from './component/Header';
 import Footer from './component/Footer';
 
 import Main from './component/Main';
+import Data from './components/data.json';
+import SelectedBeast from './components/SelectedBeast.js';
 
 class App extends React.Component {
+  constructor (props) {
+
+    super (props);
+    this.state={
+      dataarray:Data,
+      show: false,
+      item: {}
+    }
+  }
+
+  showInModal= (clickedBeast)=>{
+    this.setState({
+      
+      show: true,
+      item: clickedBeast,
+    })
+  }
+  hideInModal=()=>{
+    this.setState({
+      show:false,
+    })
+  }
 render(){
   return(
     <div>
       <Header/>
       
-      <Main/>
+      <Main 
+      BeastArray={this.state.dataarray}
+      handleClick={this.showInModal}
+      />
+
+      <SelectedBeast
+      show={this.state.show}
+      hide={this.state.hideInModal}
+      item={this.state.item}
+      />
       <Footer/>     
     </div>
   )
